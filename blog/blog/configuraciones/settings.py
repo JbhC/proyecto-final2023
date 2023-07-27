091effcff405da3b117f4affecbbb0577d7afc62
JbhC <johanacent@gmail.com>
2023-07-27 17:25:45 -0300

    Registrar, login creados

--- blog/blog/configuraciones/settings.py
+++ blog/blog/configuraciones/settings.py
@@ -78,12 +78,23 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb',
+        'USER': 'root',
+        'PASSWORD': '891314Ja!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
-}
+} 
 
 
 # Password validation
